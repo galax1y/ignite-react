f70d5cd12f8a33dbae2645aa313e82c56a1c881b
LucasWermann <lucasvw.eng@gmail.com>
2022-12-20 23:21:01 -0300

    Adicionado - Componente Sidebar
    Foi criado o componente react Sidebar e sua estilização, adicionados ícones da biblioteca Phosphor e imagens .Também foi criado um arquivo de estilização global do projeto.

--- project-one/react-apps/01-fundamentos-reactjs/src/App.jsx
+++ project-one/react-apps/01-fundamentos-reactjs/src/App.jsx
@@ -1,16 +1,28 @@
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 import { Post } from "./Post";
+
+import styles from "./App.module.css";
+import "./global.css";
 
 export function App() {
   return (
     <div>
-      <Post
-        author="Lucas Galax1y"
-        content="Why should there be content?"
-      />
-      <Post
-        author="Shakespeare"
-        content="To be or not to be, or not to be, or not to be, or not to be, or not to be"
-      />
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Lucas Galax1y"
+            content="Why should there be content?"
+          />
+          <Post
+            author="Shakespeare"
+            content="To be or not to be, or not to be, or not to be, or not to be, or not to be"
+          />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ project-one/react-apps/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1671523435843-e3cd426c24e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/50991688?v=4l"
+          alt="Profile Image"
+        />
+        <strong>Lucas Galax1y</strong>
+        <span>Full Stack Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
