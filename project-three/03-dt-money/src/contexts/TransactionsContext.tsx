1c2b2d568928345a79ffdee02d60164f15dc8535
Lucas Wermann <lw1707@hotmail.com>
2023-01-04 16:01:50 -0300

    Adicionando - Contexto de transações

---
+++ project-three/03-dt-money/src/contexts/TransactionsContext.tsx
@@ -0,0 +1,43 @@
+import { createContext, ReactNode, useEffect, useState } from 'react'
+
+export interface Transaction {
+  id: number
+  description: string
+  type: 'income' | 'outcome'
+  category: string
+  price: number
+  createdAt: string
+}
+
+interface TransactionContextType {
+  transactions: Transaction[]
+}
+
+export const TransactionsContext = createContext<TransactionContextType>(
+  {} as TransactionContextType,
+)
+
+interface TransactionProviderProps {
+  children: ReactNode
+}
+
+export function TransactionsProvider({ children }: TransactionProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3000/transactions')
+    const data = await response.json()
+
+    setTransactions(data)
+  }
+
+  useEffect(() => {
+    loadTransactions()
+  }, [])
+
+  return (
+    <TransactionsContext.Provider value={{ transactions }}>
+      {children}
+    </TransactionsContext.Provider>
+  )
+}
