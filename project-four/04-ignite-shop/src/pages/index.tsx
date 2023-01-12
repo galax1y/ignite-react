7c4adae62930d65a769e29aa0e0a49b6a597c2b7
Lucas Wermann <lw1707@hotmail.com>
2023-01-12 16:02:39 -0300

    Slider + getServerSideProps

--- project-four/04-ignite-shop/src/pages/index.tsx
+++ project-four/04-ignite-shop/src/pages/index.tsx
@@ -3,10 +3,20 @@ import { styled } from '../styles'
 import { HomeContainer, Product } from '../styles/pages/home'
 import shirt from '../assets/shirts/1.png'
 
-export default function Home() {
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
+
+export default function Home(props) {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2,
+      spacing: 48,
+    },
+  })
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className={'keen-slider'}>
+      <p>{props.list}</p>
+      <Product className="keen-slider__slide">
         <Image src={shirt} width={520} height={480} alt="" />
 
         <footer>
@@ -15,7 +25,31 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79.90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79.90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79.90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
         <Image src={shirt} width={520} height={480} alt="" />
 
         <footer>
@@ -25,4 +59,14 @@ export default function Home() {
       </Product>
     </HomeContainer>
   )
+}
+
+export const getServerSideProps = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  return {
+    props: {
+      list: [1, 2, 3],
+    },
+  }
 }
