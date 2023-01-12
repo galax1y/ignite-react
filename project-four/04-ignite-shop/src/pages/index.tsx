2e09db323541cf1ed0c05444aab1d9ffc6db4e94
Lucas Wermann <lw1707@hotmail.com>
2023-01-12 14:48:23 -0300

    Estruturando componente Home

--- project-four/04-ignite-shop/src/pages/index.tsx
+++ project-four/04-ignite-shop/src/pages/index.tsx
@@ -1,26 +1,28 @@
+import Image from 'next/image'
 import { styled } from '../styles'
-
-// Criando componente com o Stitches
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '0.75rem 2rem',
-
-  span: {
-    color: 'Red',
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'invert(100%)',
-  },
-})
+import { HomeContainer, Product } from '../styles/pages/home'
+import shirt from '../assets/shirts/1.png'
 
 export default function Home() {
   return (
-    <Button>
-      <span>Enviar</span>
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79.90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={shirt} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79.90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
