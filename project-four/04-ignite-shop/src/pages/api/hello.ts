d91b9c5bd68cf552ee6bd1d97d336e19844f1583
Lucas Wermann <lw1707@hotmail.com>
2023-01-17 14:58:41 -0300

    Checkout no Stripe com rota no Next

--- project-four/04-ignite-shop/src/pages/api/hello.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-// código server side, > não é pra causar problemas de segurança <
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.json({ message: 'Hello World' })
-}
--- project-four/04-ignite-shop/src/pages/product/[id].tsx
+++ project-four/04-ignite-shop/src/pages/product/[id].tsx
@@ -1,76 +1,107 @@
-import { GetStaticPaths, GetStaticProps } from 'next'
-import Image from 'next/image'
-import { useRouter } from 'next/router'
+import axios from 'axios'
 import Stripe from 'stripe'
+import Image from 'next/image'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import { stripe } from '../../lib/stripe'
+
 import {
-  ImageContainer,
-  ProductContainer,
-  ProductDetails,
+	ImageContainer,
+	ProductContainer,
+	ProductDetails,
 } from '../../styles/pages/product'
+import { useState } from 'react'
 
 interface ProductProps {
-  product: {
-    id: string
-    name: string
-    imageUrl: string
-    price: string
-    description: string
-  }
+	product: {
+		id: string
+		name: string
+		imageUrl: string
+		price: string
+		description: string
+		defaultPriceId: string
+	}
 }
 
 export default function Product({ product }: ProductProps) {
-  return (
-    <ProductContainer>
-      <ImageContainer>
-        <Image src={product.imageUrl} width={520} height={480} alt="" />
-      </ImageContainer>
+	// desativar o botão se ele já foi clicado
+	const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] =
+		useState(false)
 
-      <ProductDetails>
-        <h1>{product.name}</h1>
-        <span>{product.price}</span>
+	async function handleBuyProduct() {
+		try {
+			setIsCreatingCheckoutSession(true)
+			const response = await axios.post('/api/checkout', {
+				priceId: product.defaultPriceId,
+			})
 
-        <p>{product.description}</p>
+			const { checkoutUrl } = response.data
 
-        <button>Comprar agora</button>
-      </ProductDetails>
-    </ProductContainer>
-  )
+			// redirecionar usuário para uma rota externa
+			window.location.href = checkoutUrl
+
+			// se fosse para redirecionar para uma rota interna
+			// const router = useRouter()
+			// router.push('/checkout)
+		} catch (err) {
+			alert('Falha ao redirecionar ao checkout')
+			setIsCreatingCheckoutSession(false)
+		}
+	}
+
+	return (
+		<ProductContainer>
+			<ImageContainer>
+				<Image src={product.imageUrl} width={520} height={480} alt="" />
+			</ImageContainer>
+
+			<ProductDetails>
+				<h1>{product.name}</h1>
+				<span>{product.price}</span>
+
+				<p>{product.description}</p>
+
+				<button onClick={handleBuyProduct} disabled={isCreatingCheckoutSession}>
+					Comprar agora
+				</button>
+			</ProductDetails>
+		</ProductContainer>
+	)
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  return {
-    paths: [
-      {
-        params: { id: 'prod_NBDR9vzPpX6jso' },
-      },
-    ],
-    fallback: 'blocking',
-  }
+	return {
+		paths: [
+			{
+				params: { id: 'prod_NBDR9vzPpX6jso' },
+			},
+		],
+		fallback: 'blocking',
+	}
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const productId = String(params!.id)
+	const productId = String(params!.id)
 
-  const product = await stripe.products.retrieve(productId, {
-    expand: ['default_price'],
-  })
+	const product = await stripe.products.retrieve(productId, {
+		expand: ['default_price'],
+	})
 
-  const price = product.default_price as Stripe.Price
+	const price = product.default_price as Stripe.Price
 
-  return {
-    props: {
-      product: {
-        id: product.id,
-        name: product.name,
-        imageUrl: product.images[0],
-        price: new Intl.NumberFormat('pt-BR', {
-          style: 'currency',
-          currency: 'BRL',
-        }).format(price.unit_amount! / 100),
-        description: product.description,
-      },
-    },
-    revalidate: 60 * 60 * 1, // 1 hour to render again
-  }
+	return {
+		props: {
+			product: {
+				id: product.id,
+				name: product.name,
+				imageUrl: product.images[0],
+				price: new Intl.NumberFormat('pt-BR', {
+					style: 'currency',
+					currency: 'BRL',
+				}).format(price.unit_amount! / 100),
+				description: product.description,
+				defaultPriceId: price.id,
+			},
+		},
+		revalidate: 60 * 60 * 1, // 1 hour to render again
+	}
 }
