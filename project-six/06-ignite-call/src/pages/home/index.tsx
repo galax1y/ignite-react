65a2b957437f9bb5d8545faa3b84ac920cbf1776
Lucas Wermann <lw1707@hotmail.com>
2023-02-01 17:25:57 -0300

    Criando home e Componente ClaimUsernameForm

--- project-six/06-ignite-call/src/pages/home/index.tsx
+++ project-six/06-ignite-call/src/pages/home/index.tsx
@@ -1,21 +1,24 @@
 import { Container, Hero, Preview } from './styles'
-import { Heading, Text, Button } from '@ignite-ui/react'
+import { Heading, Text } from '@ignite-ui/react'
 import Image from 'next/image'
 
 import previewImage from '../../assets/app-preview.png'
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
 
 export default function Home() {
   return (
     <Container>
       <Hero>
-        <Heading size={'4xl'}>Agendamento descomplicado</Heading>
+        <Heading as="h1" size={'4xl'}>
+          Agendamento descomplicado
+        </Heading>
 
         <Text size={'xl'}>
           Conecte seu calendário e permita que as pessoas marquem agendamentos
           no seu tempo livre.
         </Text>
 
-        <Button>Criar conta com Google</Button>
+        <ClaimUsernameForm />
       </Hero>
 
       <Preview>
