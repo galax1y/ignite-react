74a5064aaeac5f79d96d5eabfacb287b38361a5e
Lucas Wermann <lw1707@hotmail.com>
2023-02-06 15:44:59 -0300

    Criando página de Schedule

---
+++ project-six/06-ignite-call/src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,64 @@
+import { prisma } from '@/lib/prisma'
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
+import { Container, UserHeader } from '../styles'
+
+interface ScheduleProps {
+  user: {
+    name: string
+    bio: string
+    avatarUrl: string
+  }
+}
+
+export default function Schedule({ user }: ScheduleProps) {
+  const router = useRouter()
+
+  console.log(router.query)
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={user.avatarUrl} />
+        <Heading>{user.name}</Heading>
+        <Text>{user.bio}</Text>
+      </UserHeader>
+    </Container>
+  )
+}
+
+// Comportamento de página estática
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: 60 * 60 * 24, // updates once a day
+  }
+}
+
+// Como username é uma rota parametrizada, precisamos gerar uma página estática
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [], // paths vazio gera a página no momento em que ela é acessada
+    fallback: 'blocking',
+  }
+}
