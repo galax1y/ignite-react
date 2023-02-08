408934d51beef0ba1de460affbcc80769f497ab8
Lucas Wermann <lw1707@hotmail.com>
2023-02-08 16:26:29 -0300

    Implementando rota de scheduling

--- project-six/06-ignite-call/src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ project-six/06-ignite-call/src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -5,6 +5,8 @@ import * as zod from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import dayjs from 'dayjs'
+import { api } from '@/lib/axios'
+import { useRouter } from 'next/router'
 
 const confirmFormSchema = zod.object({
   name: zod
@@ -33,8 +35,19 @@ export function ConfirmStep({
     resolver: zodResolver(confirmFormSchema),
   })
 
-  function handleConfirmScheduling(data: confirmFormData) {
-    console.log(data)
+  const router = useRouter()
+  const username = router.query.username
+
+  async function handleConfirmScheduling(data: confirmFormData) {
+    const { name, email, observations } = data
+    await api.post(`/users/${username}/schedule`, {
+      name,
+      email,
+      observations,
+      date: schedulingDate,
+    })
+
+    router.reload()
   }
 
   const describedDate = dayjs(schedulingDate).format('DD[ de ]MMMM[ de ]YYYY')
