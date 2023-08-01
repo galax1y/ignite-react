f6a7ccf675a242258a254a4e9a2c9c3fedda5c3c
LucasWermann <lucasvw.eng@gmail.com>
2023-08-01 14:48:57 -0300

    Add: Iniciando módulo extra de Tailwind

---
+++ tailwind-next/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Tailwind course with SSR through Next.js',
+  description: 'Developed by galax1y',
+}
+
+interface RootLayoutProps {
+  children: React.ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ tailwind-next/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className='bg-slate-900 text-slate-100 p-4 text-lg'>
+      <h1 className='text-galax1y text-5xl'>Hello</h1>
+      <h2>galax1y</h2>
+      <p>World</p>
+    </div>
+  )
+}
