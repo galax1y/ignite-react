4a040ea9d4074a7b7aceb9e38502abd6418da602
LucasWermann <lucasvw.eng@gmail.com>
2023-08-01 15:03:08 -0300

    Add: Uso de seletores enabled, before, disabled

--- tailwind-next/src/app/page.tsx
+++ tailwind-next/src/app/page.tsx
@@ -1,9 +1,15 @@
 export default function Home() {
   return (
-    <div className='bg-slate-900 text-slate-100 p-4 text-lg'>
-      <h1 className='text-galax1y text-5xl'>Hello</h1>
+    <div className='bg-slate-900 text-slate-100 p-8 h-screen'>
+      <h1 className='font-bold text-5xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500 before:flex'>
+        Hello
+      </h1>
       <h2>galax1y</h2>
       <p>World</p>
+
+      <button disabled className='bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed'>
+        Sign in
+      </button>
     </div>
   )
 }
