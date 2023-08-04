6cb83ad43845b1ffa43c28a24991cd1bda812019
LucasWermann <lucasvw.eng@gmail.com>
2023-08-03 21:41:42 -0300

    Refactor: Input c/ pattern de composição

--- tailwind-next/src/components/Sidebar/sidebar.tsx
+++ tailwind-next/src/components/Sidebar/sidebar.tsx
@@ -14,19 +14,20 @@ import {
 } from 'lucide-react'
 import { UsedSpaceWidget } from './used-space-widget'
 import { Profile } from './profile'
+import * as Input from '../input'
 
 export function Sidebar() {
   return (
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
-      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          placeholder="Search"
-          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-0"
-        />
-      </div>
+      {/* Pattern de Composição */}
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
 
       <nav className="space-y-0.5">
         <NavItem title="Home" icon={Home} />
---
+++ tailwind-next/src/components/input.tsx
@@ -0,0 +1,33 @@
+import { ComponentProps } from 'react'
+
+// Pattern de composição
+
+// interface InputPrefixProps extends ComponentProps<'div'> -> Extende todas as propriedades de um elemento html, seja div, button, input, qualquer...
+
+type InputPrefixProps = ComponentProps<'div'>
+
+export function Prefix(props: InputPrefixProps) {
+  return <div {...props} />
+}
+
+type InputControlProps = ComponentProps<'input'>
+
+export function Control(props: InputControlProps) {
+  return (
+    <input
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-0"
+      {...props}
+    />
+  )
+}
+
+type InputRootProps = ComponentProps<'div'>
+
+export function Root(props: InputRootProps) {
+  return (
+    <div
+      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      {...props}
+    />
+  )
+}
