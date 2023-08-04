8ae4e007abb2691e4ba7dd64bbe57d31f3964528
LucasWermann <lucasvw.eng@gmail.com>
2023-08-04 16:48:18 -0300

    Add: Radix Tabs p/ parte principal da página

--- tailwind-next/src/app/layout.tsx
+++ tailwind-next/src/app/layout.tsx
@@ -16,7 +16,7 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
+    <html lang="en" className="antialiased">
       <body className={inter.className}>
         <div className="grid min-h-screen grid-cols-app">
           <Sidebar />
