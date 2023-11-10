ac1663eac53705fc604432e234bed56b333dc779
galax1y <lucasvw.eng@gmail.com>
2023-11-09 21:28:34 -0300

    Add: Responsive sidebar w/ Radix Collapsible component

--- tailwind-next/src/app/layout.tsx
+++ tailwind-next/src/app/layout.tsx
@@ -1,7 +1,7 @@
-import { Sidebar } from '@/components/Sidebar/sidebar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Sidebar } from '@/components/Sidebar/sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,7 +20,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body className={inter.className}>
         <div className="grid min-h-screen grid-cols-app">
           <Sidebar />
-          <main className="px-8 pb-12 pt-8">{children}</main>
+          <main className="px-4 pb-12 pt-24 lg:col-start-2 lg:px-8 lg:pt-8">
+            {children}
+          </main>
         </div>
       </body>
     </html>
